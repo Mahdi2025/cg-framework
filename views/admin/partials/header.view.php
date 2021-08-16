06f764c8e71ef82c805391ada0282678d07c3928
Mahdi2025 <mahdi.benameur1@gmail.com>
2021-08-16 10:49:56 +0200

    Something

--- views/admin/partials/header.view.php
+++ views/admin/partials/header.view.php
@@ -3,7 +3,6 @@
         <div class="col-md-10">
             <h1>Admin</h1>
         </div>
-
         <div class="col-md-2 text-right pr-4">
             <a href="logout">Uitloggen</a>
         </div>
